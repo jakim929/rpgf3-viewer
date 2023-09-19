2ac4aec44e607902a2ec129bde2d092b009d6a0a
James Kim <jakim929@gmail.com>
2023-09-19 18:40:47 -0400

    add usequery

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { WagmiConfig } from 'wagmi'
 import './App.css'
 import { ConnectButton, RainbowKitProvider } from '@rainbow-me/rainbowkit'
 import { chains, wagmiConfig } from '@/wagmi'
 
+const queryClient = new QueryClient()
+
 function App() {
   return (
-    <WagmiConfig config={wagmiConfig}>
-      <RainbowKitProvider chains={chains}>
-        Hello world <ConnectButton />
-      </RainbowKitProvider>
-    </WagmiConfig>
+    <QueryClientProvider client={queryClient}>
+      <WagmiConfig config={wagmiConfig}>
+        <RainbowKitProvider chains={chains}>
+          Hello world <ConnectButton />
+        </RainbowKitProvider>
+      </WagmiConfig>
+    </QueryClientProvider>
   )
 }
 
